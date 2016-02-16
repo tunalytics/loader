5f6d0f125f06e63736886dd37f47653846ac6e42
IlyaGubarev <ilya.gubarev@gmail.com>
2016-02-16 19:30:29 +0100

    Application test implementation added

--- src/main/scala/org/tunalytics/loader/Application.scala
+++ src/main/scala/org/tunalytics/loader/Application.scala
@@ -1,8 +1,36 @@
 package org.tunalytics.loader
 
+import backtype.storm.Config
+import backtype.storm.LocalCluster
+import backtype.storm.topology.TopologyBuilder
+import backtype.storm.utils.Utils
+
+import org.tunalytics.loader.topology.signals.SignalBolt
+import org.tunalytics.loader.topology.signals.SignalSpout
+
 object Application {
 
     def main(arguments: Array[String]) {
-        // TODO: build and submit the topology
+        submitSignalTopology()
+        // TODO: implement real topology building and submitting
+    }
+
+    private def submitSignalTopology() {
+
+        val config = new Config
+        config.setDebug(false)
+
+        val builder = new TopologyBuilder
+        builder.setSpout("signalSpout", new SignalSpout, 1)
+        builder.setBolt("signalBolt", new SignalBolt, 1)
+                .shuffleGrouping("signalSpout")
+
+        val cluster = new LocalCluster
+        cluster.submitTopology("testTopology", config, builder.createTopology())
+
+        Utils.sleep(30000);
+
+        cluster.killTopology("testTopology");
+        cluster.shutdown();
     }
 }
